77da7c91500aabc90ae6ef07bc226e25afa75ae1
Manuel Alva Barrera <jalvabarrera1@nait.ca>
2024-07-19 14:26:57 -0600

    Re-created NavBar File

--- pages/applications.js
+++ pages/applications.js
@@ -1,8 +1,26 @@
 import { Box } from "@mui/material";
 import NavBar from "@/components/NavBar";
 import Container from "@mui/material/Container";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Applications() {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return (
+      <main>
+        <NavBar />
+        <Container>
+          <Box
+            sx={{ display: "flex", paddingTop: 4, justifyContent: "center" }}
+          >
+            <CircularProgress />
+          </Box>
+        </Container>
+      </main>
+    );
+  }
+
   return (
     <main>
       <NavBar />
