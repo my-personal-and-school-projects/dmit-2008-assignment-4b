296c401c657460e120e53e768468a4e281651f82
Manuel Alva Barrera <jalvabarrera1@nait.ca>
2024-07-19 14:21:56 -0600

    Created Apps Page

--- components/apply/ApplyJobForm.js
+++ components/apply/ApplyJobForm.js
@@ -1,8 +1,19 @@
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 export default function ApplyJobForm({ job, submitCallback }) {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function onHandleSubmitAppForm() {
+    if (fullName === "") {
+    }
+    if (email === "") {
+    }
+  }
+
   return (
     <form style={{ width: `90%` }}>
       <Stack direction="column" spacing={2}>
---
+++ pages/applications.js
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import NavBar from "@/components/NavBar";
+import Container from "@mui/material/Container";
+
+export default function Applications() {
+  return (
+    <main>
+      <NavBar />
+      <Container>
+        <Box>Applications Page</Box>
+      </Container>
+    </main>
+  );
+}
